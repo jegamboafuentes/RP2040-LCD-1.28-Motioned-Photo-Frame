d975c2e64946e75af755e9bc40b7706dc434ab79
Jorge Enrique Gamboa Fuentes <snake2209@hotmail.com>
2024-02-16 19:27:59 -0500

    V1 working

--- V1/boot.py
+++ V1/boot.py
@@ -1,6 +1,8 @@
 from machine import Pin, SPI, I2C
+import time
 import gc9a01
 from sensors import QMI8658
+import random  # Import the random module
 
 # Corrected setup for QMI8658 gyroscope using individual SDA and SCL pin numbers
 I2C_SDA = 6
@@ -16,7 +18,7 @@ def main():
         cs=Pin(9, Pin.OUT),
         dc=Pin(8, Pin.OUT),
         backlight=Pin(25, Pin.OUT),
-        rotation=0)
+        rotation=2)
 
     tft.init()
     tft.fill(gc9a01.WHITE)
@@ -25,7 +27,8 @@ def main():
     qmi8658 = QMI8658(I2C_SDA, I2C_SCL)
 
     #image_paths = ["logo.jpg", "test.jpg"]
-    image_paths = ["logo230.jpg","test.jpg"]
+    image_paths = ["logo230.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg"]
+    current_image_path = random.choice(image_paths)
     current_image_index = 0
 
     image_width = 230
@@ -33,7 +36,7 @@ def main():
     x = (240 - image_width) // 2
     y = (240 - image_height) // 2
 
-    def read_gyroscope(acc_threshold=0.5, gyro_threshold=20):
+    def read_gyroscope(acc_threshold=1, gyro_threshold=20):
         """
         Check for significant movement using the gyroscope and accelerometer.
         Returns True if significant movement is detected, False otherwise.
@@ -48,8 +51,22 @@ def main():
         if acc_magnitude < 1 or acc_magnitude > 1.3:
             return True
         return False
+#RANDOM ORDER
+    while True:
+        if read_gyroscope():
+            # Keep selecting a new image path until it's different from the current one
+            new_image_path = current_image_path
+            while new_image_path == current_image_path:
+                new_image_path = random.choice(image_paths)
+            current_image_path = new_image_path
+            tft.fill(gc9a01.WHITE)
+    
+        # Display the current (or newly selected) image
+        tft.jpg(current_image_path, x, y, gc9a01.FAST)
+        time.sleep(1)
 
-
+'''
+#IN ORDER
     while True:
         if read_gyroscope():
             current_image_index = (current_image_index + 1) % len(image_paths)
@@ -57,8 +74,11 @@ def main():
         
         tft.jpg(image_paths[current_image_index], x, y, gc9a01.FAST)
         time.sleep(1)
+'''
+
 
 main()
+
 
 
 
